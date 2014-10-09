b11dea8276f8c14086e3b9ea67e806dfcc6acb2c
Mikhail <ext-mikhail.podobivskiy@lindorff.com>
2014-10-09 23:26:41 +0300

    work around enemies creation

--- game.js
+++ game.js
@@ -24,7 +24,11 @@ sprites.src = "images/sprites.png";
 var player;
 var enemies = [];
 
-var isPlaying
+var spawnInterval;
+var spawnTime = 12000;
+var spawnAmount = 5;
+
+var isPlaying;
 
 var requestAnimFrame = window.requestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
@@ -66,6 +70,17 @@ function init() {
     document.addEventListener("keyup", checkKeyUp, false);
 };
 
+function startCreatingEnemies() {
+    stopCreatinEnemies();
+    spawnInterval = setInterval(function() {
+        spawnEnemy(spawnAmount);
+    }, spawnTime);
+}
+
+function stopCreatinEnemies() {
+    clearInterval(spawnInterval);
+}
+
 function spawnEnemy(count) {
     for (var i = 0; i < count; i++) {
         enemies[i] = new Enemy();
@@ -83,6 +98,7 @@ function loop() {
 function startLoop() {
     isPlaying = true;
     loop();
+    startCreatingEnemies();
 };
 
 function stopLoop() {
@@ -192,7 +208,7 @@ function Enemy() {
     this.srcX = 30;
     this.srcY = 0;
     this.drawX = getRandomInt(gameWidth, gameWidth + gameWidth);
-    this.drawY = getRandomInt(0, gameHeight-30);
+    this.drawY = getRandomInt(0, gameHeight - 30);
     this.width = 30;
     this.height = 30;
 
@@ -206,12 +222,14 @@ Enemy.prototype.draw = function() {
 
 Enemy.prototype.update = function() {
     this.drawX -= this.speed;
-    if (this.drawX < -30) {
-        this.drawX = getRandomInt(gameWidth, gameWidth + gameWidth);
-        this.drawY = getRandomInt(0, gameHeight - 30);
+    if (this.drawX + this.width < 0) {
+        this.destroy();
     };
 };
 
+Enemy.prototype.destroy = function () {
+	enemies.splice(enemies.indexOf(this),1)
+	}
 
 function clearCtxPl() {
     ctxPl.clearRect(0, 0, gameWidth, gameHeight);
